3c5aca6b49d52bc29a19acbdc2928bbd1351bae0
Mozhexxian <zhangzhexian@outlook.com>
2017-08-08 17:23:19 +0800

    Move GUI directory

---
+++ appJar_GUI_linux/appJar/code/label_image.py
@@ -0,0 +1,217 @@
+# Copyright 2016 The TensorFlow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+"""Simple image classification with Inception.
+
+Run image classification with your model.
+
+This script is usually used with retrain.py found in this same
+directory.
+
+This program creates a graph from a saved GraphDef protocol buffer,
+and runs inference on an input JPEG image. You are required
+to pass in the graph file and the txt file.
+
+It outputs human readable strings of the top 5 predictions along with
+their probabilities.
+
+Change the --image_file argument to any jpg image to compute a
+classification of that image.
+
+Example usage:
+python label_image.py --graph=retrained_graph.pb
+  --labels=retrained_labels.txt
+  --image=flower_photos/daisy/54377391_15648e8d18.jpg
+
+NOTE: To learn to use this file and retrain.py, please see:
+
+https://codelabs.developers.google.com/codelabs/tensorflow-for-poets
+"""
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import argparse
+import thread
+import sys
+
+sys.path.append("../../")
+# import the library
+from appJar import gui
+
+import tensorflow as tf
+from PIL import Image
+from io import BytesIO
+
+app = gui()
+
+parser = argparse.ArgumentParser()
+
+'''
+parser.add_argument(
+    '--image', required=True, type=str, help='Absolute path to image file.')
+'''
+parser.add_argument(
+    '--num_top_predictions',
+    type=int,
+    default=5,
+    help='Display this many predictions.')
+'''
+parser.add_argument(
+    '--graph',
+    required=True,
+    type=str,
+    help='Absolute path to graph file (.pb)')
+parser.add_argument(
+    '--labels',
+    required=True,
+    type=str,
+    help='Absolute path to labels file (.txt)')
+'''
+parser.add_argument(
+    '--output_layer',
+    type=str,
+    default='final_result:0',
+    help='Name of the result operation')
+parser.add_argument(
+    '--input_layer',
+    type=str,
+    default='DecodeJpeg/contents:0',
+    help='Name of the input operation')
+
+FLAGS, unparsed = parser.parse_known_args()
+FLAGS.graph = app.openBox(title="Choose graph", dirName=None, fileTypes=[("tensorflow graph","*.pb")], asFile=False)
+FLAGS.labels = app.openBox(title="Choose labels", dirName=None, fileTypes=[("text file","*.txt")], asFile=False)
+
+
+#takes in Image object and returns it in JPEG format
+def convertToJpeg(im):
+    with BytesIO() as f:
+        im.save(f, format='JPEG')
+        return f.getvalue()
+
+def load_image(filename):
+  """Read in the image_data to be classified."""
+  image = Image.open(filename)
+  return convertToJpeg(image)
+
+
+def load_labels(filename):
+  """Read in labels, one label per line."""
+  return [line.rstrip() for line in tf.gfile.GFile(filename)]
+
+
+def load_graph(filename):
+  """Unpersists graph from file as default graph."""
+  with tf.gfile.FastGFile(filename, 'rb') as f:
+    graph_def = tf.GraphDef()
+    graph_def.ParseFromString(f.read())
+    tf.import_graph_def(graph_def, name='')
+
+def run_graph(image_data, labels, input_layer_name, output_layer_name,
+              num_top_predictions):
+  with tf.Session() as sess:
+    # Feed the image_data as input to the graph.
+    #   predictions  will contain a two-dimensional array, where one
+    #   dimension represents the input image count, and the other has
+    #   predictions per class
+    softmax_tensor = sess.graph.get_tensor_by_name(output_layer_name)
+    predictions, = sess.run(softmax_tensor, {input_layer_name: image_data})
+
+    # Sort to show labels in order of confidence
+    top_k = predictions.argsort()[-num_top_predictions:][::-1]
+    for node_id in top_k:
+      human_string = labels[node_id]
+      score = predictions[node_id]
+      print('%s (score = %.5f)' % (human_string, score))
+      if(node_id==0):
+        app.setMessage("Classification result_1", '%s (score = %.5f)' % (human_string, score))
+      else:
+        app.setMessage("Classification result_2", '%s (score = %.5f)' % (human_string, score))    
+  return
+
+
+def main(argv):
+  """Runs inference on an image."""
+  if argv[1:]:
+    raise ValueError('Unused Command Line Args: %s' % argv[1:])
+
+  if not tf.gfile.Exists(FLAGS.image):
+    tf.logging.fatal('image file does not exist %s', FLAGS.image)
+
+  if not tf.gfile.Exists(FLAGS.labels):
+    tf.logging.fatal('labels file does not exist %s', FLAGS.labels)
+
+  if not tf.gfile.Exists(FLAGS.graph):
+    tf.logging.fatal('graph file does not exist %s', FLAGS.graph)
+
+  # load image
+  image_data = load_image(FLAGS.image)
+
+  # load labels
+  labels = load_labels(FLAGS.labels)
+
+  # load graph, which is stored in the default session
+  load_graph(FLAGS.graph)
+
+  run_graph(image_data, labels, FLAGS.input_layer, FLAGS.output_layer,
+            FLAGS.num_top_predictions)
+  
+  return
+
+def classify(image,unparsed):
+  
+  tf.app.run(main=main, argv=sys.argv[:1]+unparsed)
+
+if __name__ == '__main__':
+  '''
+  python label_image.py --image /home/zhexian/Downloads/appJar/images/SOB_B_A-14-22549AB-40-001.png --graph /home/zhexian/Downloads/BreastCancerClassifier/output_graph.pb --labels /home/zhexian/Downloads/BreastCancerClassifier/output_labels.txt
+  '''
+
+  def press(btn):
+    image = app.openBox(title="Choose image", dirName=None, fileTypes=None, asFile=False)
+    app.reloadImage("cancer_image", image)
+    app.setMessage("Classification result_1", "Please wait...")
+    app.setMessage("Classification result_2", "Please wait...")
+    FLAGS.image = image
+    thread.start_new_thread(classify, (image,unparsed))
+  
+  
+  app.addLabel("title", "Breast Cancer Classifier")
+  app.getLabelWidget("title").config(font=("Sans Serif", "18", "bold"))
+  
+
+  app.startLabelFrame("Image Preview")
+
+  app.addImage("cancer_image", "../images/SOB_B_A-14-22549AB-40-001.png")
+
+  app.stopLabelFrame()
+
+  app.addButton("Choose Image & Classify", press)
+
+  app.addLabel("result", "CLassification Result")
+  app.getLabelWidget("result").config(font=("Sans Serif", "18", "bold"))
+
+  app.addMessage("Classification result_1", "Please wait...")
+  app.addMessage("Classification result_2", "Please wait...")
+  app.setMessageWidth("Classification result_1", 500)
+  app.setMessageWidth("Classification result_2", 500)
+
+  app.setStretch("both")
+
+  app.setFont(14)
+
+  app.go()
+
+
